5290245c7eceaf2c58f5ff09c1d262ebb70925cd
SiraYsia <123924696+SiraYsia@users.noreply.github.com>
2023-06-27 17:31:30 -0400

    Update recomend.py

--- recomend.py
+++ recomend.py
@@ -1,78 +1,61 @@
 import requests 
-import sqlite3 as db 
+#import sqlite3 as db 
 import pandas as pd
+import os
+import json
+import sqlalchemy as db 
 
 
 #API integration 
 #Make a request to fetch movie data from (---) API
 
-def fetch_movie_data():
-    api_key = '  '
-    url = "       " 
-    response = requests.get(url)
+def make_google_books_api_request(subject, num_books):
+    url = 'https://www.googleapis.com/books/v1/volumes'
+    api_key = os.environ.get('BOOKS_API_KEY')
+
+    query = f'subject:{subject}'
+
+
+    params = {
+        'q': 'subject:{subject}',
+        'orderBy': 'rating',
+        'maxResults': num_books, 
+        'key': api_key
+    }
+
+    response = requests.get(url, params = params)
     data = response.json()
     return data
 
-#store movie data in the database
-def store_movies_in_database(movies):
-    engine = db.create_engine('sqlite:///move_recomendation.db')
-    con = engine.connect()
 
-    #Defind the table structure 
+def extract_book_titles(data):
+    book_titles = [item['volumeInfo']['title'] for item in data['items']]
+    return book_titles
 
-    table = db.Table(
-        'movies',
-        db.column('title', db.String),
-        db.column('genere', db.String),
-        db.column('rating', db.Float), 
-    )
+def save_book_titles_to_database(book_titles, database_name):
+    data_frame = pd.DataFrame({'book_title': book_titles})
+    engine = db.create_engine(f'sqlite:///{database_name}.db')
+    data_frame.to_sql('table_name', con=engine, if_exists='replace', index=False)
 
-    #create the table if it does not exsist
-    table.create(engine, checkfirst=True)
+def retrieve_from_database(database_name):
+    engine = db.create_engine(f'sqlite:///{database_name}.db')
+    with engine.connect() as connection:
+        query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+        return pd.DataFrame(query_result)
 
-    #insert movi data into the table
+subject = input("Enter a subject for a book recomendation: ")
+num_books = int(input("Enter the number books to be suggested: "))
 
-    for movie in movies:
-        query = table.insert().values(title = movie['title'],
-        genere = movie['genere'], 
-        rating = movie['rating'])
-
-        #somehow execute the quert 
-    #close connection 
+data = make_google_books_api_request(subject, num_books)
+book_titles = extract_book_titles(data)
+save_book_titles_to_database(book_titles, 'database_name')
+retrive_titles = retrieve_from_database('database_name')
+print(retrive_titles)
 
 
-    #retrieve recomended movies from the data base 
-
-    def get_recomended_movies(fav_mov):
-        engine = db.create_engine('sqlite://movie_recomendation.db')
-        con = engine.connect()
-
-        #Build the query to retrieve recomended moves where movies matches favorite movie's genere 
-
-        #execute the query and fetch the result 
-
-        return result 
-
-
-    def main():
-
-        #fetch movie data from API
-        movie_data = fetch_movie_data
-
-        #store movie data in the database
-        store_movies_in_database(movie_data)
-
-        #user input 
-        favoriate_movie = 'title' = input ("Enter your favoriate movie: ")
-
-        #Get recomended movies based on user's favoriate movie
-
-        recomended_movies = get_recomended_movies(favoriate_movie)
-
-        #Display recomeded movies
-
-        print("recomende movies: ")
-        for movies in recomend_movies:
-            print(movie.title, movie.genere, movie.rating)
-    if __name__ == '__main__':
-        main()
+# 1. Prompt for subject
+# 2. Ask for a genre 
+# 3. Ask for prefrences like author 
+# 4. ask how many books they want to be suggested 
+# 4. Suggest book that match the genre based on high rating
+# 5. Ask to choose one book suggesting that they liked very much and use it in future suggestion despite ratings 
